b19eea562e587c92b06f0cdf345c15096643e8cb
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-01-03 21:55:16 +0900

    Parse command len

--- minish.c
+++ minish.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 const ssize_t BUF_SIZE = 4096;
+
+int command_len(char *buf)
+{
+  for (int i = 0; i < sizeof(buf); i++)
+  {
+    char c = buf[i];
+    if (c == ' ' || c == '\0' || c == '\n')
+    {
+      return i - 1;
+    }
+  }
+
+  return -1;
+}
 
 int main()
 {
@@ -13,10 +28,22 @@ int main()
 
     fgets(buf, sizeof(buf), stdin);
 
-    if (!strncmp(buf, "echo ", 5))
+    int len = command_len(buf);
+
+    char *command = (char *)malloc(sizeof(char) * (len + 1));
+    strncpy(command, buf, len + 1);
+    command[len + 1] = '\0';
+
+    if (!strcmp(command, "echo"))
     {
-      printf("%s", buf + 5);
+      printf("%s", buf + len + 1);
     }
+    else
+    {
+      fprintf(stdout, "%s: command not found\n", command);
+    }
+
+    free(command);
   }
 
   return 0;
